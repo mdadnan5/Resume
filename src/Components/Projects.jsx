c1859793c1b8c69f4976a41fc8304f954e463040
Mohammad Adnan <90039274+mdadnan5@users.noreply.github.com>
2024-10-20 10:53:00 +0530

    Add files via upload

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,32 @@
+import { Description } from "./Description";
+
+export const Projects = ({ Projects }) => {
+  return (
+    <section className="Projects-experience section" id="Projects">
+      <h2 className="section-title">Projects</h2>
+      <div className="experience__container bd-grid">
+        {Projects?.map((proyect) => (
+          <Proyect key={proyect.company} {...proyect} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+const Proyect = ({ name, company, period, description }) => {
+  return (
+    <div className="experience__content">
+      <div className="experience__time">
+        <span className="experience__rounder"></span>
+        <span className="experience__line"></span>
+      </div>
+      <div className="experience__data bd-grid">
+        <h3 className="experience__title">
+          {name} - {company}
+        </h3>
+        <span className="experience__proyect">{period}</span>
+        {description.map((desc, i) => <Description key={i} desc={desc} />)}
+      </div>
+    </div>
+  );
+};
---
+++ src/Schemas/Data.js
@@ -0,0 +1,135 @@
+export const Data = {
+  profile: {
+    name: "Mohammad Adnan",
+    ocupation: "Sr. Software Engineer",
+    location: "Noida, U.P, India",
+    email: "adnanasharaf786@gmail.com",
+    telephone: "+91 9919917878",
+    image: "images/Adnan.png",
+  },
+  aboutMe: {
+    label: "Profile",
+    description:
+    "I am a frontend developer based in Noida with expertise in HTML5, CSS3, JavaScript (ES6+), React.js, Redux.js, and Next.js. I have completed 10+ projects and possess strong proficiency in DOM manipulation, the JavaScript object model, and React’s core principles. Experienced with Redux, RESTful APIs, and tools like NPM, I excel at translating business requirements into technical solutions, with a focus on benchmarking, optimization, and version control."
+  },
+  skills: {
+    technicalLabel: "My Skills",
+    softLabel: "Other Skills",
+    technicalSkills: [
+      "JavaScript",
+      "React",
+      "Next",
+      "Redux",
+      "Context API",
+      "HTML5",
+      "CSS3",
+      "SASS",
+      "Git",
+      "Tailwind CSS",
+      "Bootstrap",
+      "Mui",
+    ],
+    softSkills: [
+      "Docker",
+      "Jenkins",
+      "Figma",
+      "Canva",
+      "Jira",
+      "Slack"
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Visit my LinkedIn",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/adnanasharaf",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Visit my Github",
+        name: "github",
+        url: "https://github.com/mdadnan5",
+        className: "bx bxl-github social__icon",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "Sr. Software Engineer",
+        period: "Aug 2022 - Present",
+        company: "Plutos ONE Pvt. Ltd.",
+        description: [
+          "Built plutos ONE web and various banks and brands campaigns (BOB, SBI, HDFC, RuPay, BHIM, idfc, etc.) using NextJS",
+          "Developed poll on news campaigns like news9live and zee news",
+          "Developed and integrated APIs to enhance data flow and functionality. Conducted API performance optimization using profiling tools, resulting in a 15% reduction in page load times.",
+          "Developing software solutions by studying information needs, conferring with users, studying systems flow, data usage, and work processes",
+          "Led the development and integration of a voucher management system, enabling seamless voucher redemption through REST APIs, increasing user engagement by 25%.",
+          "Ensured data privacy and security by implementing encryption protocols and OAuth2 authentication for API access; also contributed to Plutos One Web App development.",
+          "Established a unified login and registration flow for all campaigns and internal tools using Firebase authentication",
+          " Implemented BBPS (Bharat Bill Payment System) for bill payments provided by NPCI, ensuring seamless payment processes.",
+        ],
+      },
+      {
+        title: "Frontend Developer",
+        period: "July 2021 - Aug 2022",
+        company: "Triline Infotech Pvt. Ltd.",
+        description: [
+          "Developed a schema-driven UI for contracts management, enabling payers and providers to input their terms and compare benefits. This UI dynamically generates the appropriate interface elements based on the schema provided by the backend..",
+          "Created user interfaces using JavaScript frameworks and integrated APIs to enhance application functionality.",
+          "Translated Figma designs into production-ready JSX code for React applications, ensuring high-quality UI implementation.",
+        ],
+      },
+      {
+        title: "Full Stack Web development | Internship & Certification",
+        period: "Jan 2021 - June 2021",
+        company: "Newton School",
+        description: [
+          "Participated in various challenges organized by the platform.",
+          "Worked on MERN Stack and various projects like Quiz app, Weather app, Movie Bucket app, Tic Tac Toe etc.",
+        ],
+      },
+    
+    ],
+    academic: [
+      {
+        career: "Bachelor of Technology (B.Tech) in Electronics Engineering",
+        date: "July 2016 - June 2019",
+        institution: "Dr. A P J Abdul Kalam Technical University Lucknow",
+      },
+      {
+        career: "Bachelor of Science (B.Sc) in PCM",
+        date: "July 2013 - June 2016",
+        institution: "Chhatrapati Shahu Ji Maharaj University, Kanpur",
+      },
+    ],
+    Projects: [
+      {
+        name: "Weather Application",
+        company: "",
+        period: "",
+        description: [
+          "I developed a Weather Application provides real-time weather updates and forecasts for any location worldwide. It integrates with a weather API to display current conditions like temperature, humidity, wind speed. Users can search for cities and enable location-based weather updates.",
+        ],
+      },
+      {
+        name: "Digital and Analog Watch",
+        company: "",
+        period: "",
+        description: [
+          "I developed a Digital and Analog Watch project is designed to combine the best features of both traditional analog watches and modern digital displays. This hybrid watch offers the elegance of an analog face with rotating hour and minute hands, paired with a digital interface for additional functionality, precision, and ease of use.",
+        ],
+      },
+      {
+        name: "Portfolio",
+        company: "",
+        period: "",
+        description: [
+          "I developed a personal portfolio website that showcases my skills, projects, and professional experience. The site includes detailed sections for my resume, an interactive contact form, and all essential information, such as email and phone number, making it easy for potential clients and collaborators to reach out. It provides a clear and concise overview of my work, highlighting my expertise in Software development and modern web technologies.",
+        ],
+      }
+    ],
+  },
+};
